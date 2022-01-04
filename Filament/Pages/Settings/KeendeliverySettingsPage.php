1fb6af109f7f061e6f5f94cf9527371e3d48c8b9
Robin van Maasakker <robin@qubiqx.com>
2022-01-04 14:16:34 +0100

    Update

--- Filament/Pages/Settings/KeendeliverySettingsPage.php
+++ Filament/Pages/Settings/KeendeliverySettingsPage.php
@@ -12,7 +12,7 @@ use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
 use Qubiqx\QcommerceCore\Classes\Sites;
 use Qubiqx\QcommerceCore\Models\Customsetting;
-use Qubiqx\QcommerceEcommerceChannable\Classes\Channable;
+use Qubiqx\QcommerceEcommerceKeenDelivery\Classes\KeenDelivery;
 
 class KeendeliverySettingsPage extends Page implements HasForms
 {
@@ -49,40 +49,40 @@ class KeendeliverySettingsPage extends Page implements HasForms
         foreach ($sites as $site) {
             $schema = [
                 Placeholder::make('label')
-                    ->label("Channable voor {$site['name']}")
-                    ->content('Activeer Channable.')
+                    ->label("KeenDelivery voor {$site['name']}")
+                    ->content('Activeer KeenDelivery.')
                     ->columnSpan([
                         'default' => 1,
                         'lg' => 2,
                     ]),
                 Placeholder::make('label')
-                    ->label("Channable is " . (! Customsetting::get('channable_connected', $site['id'], 0) ? 'niet' : '') . ' geconnect')
+                    ->label("KeenDelivery is " . (! Customsetting::get('channable_connected', $site['id'], 0) ? 'niet' : '') . ' geconnect')
                     ->content(Customsetting::get('channable_connection_error', $site['id'], ''))
                     ->columnSpan([
                         'default' => 1,
                         'lg' => 2,
                     ]),
                 TextInput::make("channable_api_key_{$site['id']}")
-                    ->label('Channable API key')
+                    ->label('KeenDelivery API key')
                     ->rules([
                         'max:255',
                     ]),
                 TextInput::make("channable_company_id_{$site['id']}")
-                    ->label('Channable company ID')
+                    ->label('KeenDelivery company ID')
                     ->rules([
                         'max:255',
                     ]),
                 TextInput::make("channable_project_id_{$site['id']}")
-                    ->label('Channable project ID')
+                    ->label('KeenDelivery project ID')
                     ->rules([
                         'max:255',
                     ]),
                 Toggle::make("channable_feed_enabled_{$site['id']}")
-                    ->label('Channable feed aanzetten'),
+                    ->label('KeenDelivery feed aanzetten'),
                 Toggle::make("channable_order_sync_enabled_{$site['id']}")
-                    ->label('Order uit Channable naar webshop syncen'),
+                    ->label('Order uit KeenDelivery naar webshop syncen'),
                 Toggle::make("channable_stock_sync_enabled_{$site['id']}")
-                    ->label('Voorraad vanuit webshop naar Channable syncen'),
+                    ->label('Voorraad vanuit webshop naar KeenDelivery syncen'),
             ];
 
             $tabs[] = Tab::make($site['id'])
@@ -110,11 +110,11 @@ class KeendeliverySettingsPage extends Page implements HasForms
             Customsetting::set('channable_feed_enabled', $this->form->getState()["channable_feed_enabled_{$site['id']}"], $site['id']);
             Customsetting::set('channable_order_sync_enabled', $this->form->getState()["channable_order_sync_enabled_{$site['id']}"], $site['id']);
             Customsetting::set('channable_stock_sync_enabled', $this->form->getState()["channable_stock_sync_enabled_{$site['id']}"], $site['id']);
-            Customsetting::set('channable_connected', Channable::isConnected($site['id']), $site['id']);
+            Customsetting::set('channable_connected', KeenDelivery::isConnected($site['id']), $site['id']);
         }
 
-        $this->notify('success', 'De Channable instellingen zijn opgeslagen');
+        $this->notify('success', 'De KeenDelivery instellingen zijn opgeslagen');
 
-        return redirect(ChannableSettingsPage::getUrl());
+        return redirect(KeenDeliverySettingsPage::getUrl());
     }
 }
