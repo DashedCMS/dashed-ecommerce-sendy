506d4010e4ee6d3d88bcf25b76cd10f7fafd63de
Robin van Maasakker <robin@qubiqx.com>
2023-09-15 17:58:01 +0200

    Update

--- src/Controllers/KeendeliveryController.php
+++ src/Controllers/KeendeliveryController.php
@@ -18,11 +18,11 @@ class KeendeliveryController extends FrontendController
             $fileName = '/dashed/keendelivery/labels/labels-' . time() . '.pdf';
             Storage::disk('dashed')->put($fileName, base64_decode($response['labels']));
             foreach ($keendeliveryOrders as $keendeliveryOrder) {
-                $keendeliveryOrder->label_printed = 1;
+//                $keendeliveryOrder->label_printed = 1;
                 $keendeliveryOrder->save();
             }
 
-            return Storage::download($fileName);
+            return Storage::disk('dashed')->download($fileName);
         } else {
             echo "<script>window.close();</script>";
         }
