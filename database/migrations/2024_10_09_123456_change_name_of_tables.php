f1cccb25c11fc0f9957c88a81915c090b345e3a2
Robin van Maasakker <robin@qubiqx.com>
2025-08-19 11:06:45 +0200

    update

--- database/migrations/2024_10_09_123456_change_name_of_tables.php
+++ database/migrations/2024_10_09_123456_change_name_of_tables.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration {
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,14 +14,14 @@ return new class extends Migration {
         Schema::rename('dashed__keendelivery_shipping_methods', 'dashed__sendy_shipping_methods');
         Schema::rename('dashed__keendelivery_shipping_method_services', 'dashed__sendy_shipping_method_services');
         Schema::rename('dashed__keendelivery_shipping_method_service_options', 'dashed__sendy_shipping_method_service_options');
-        Schema::table('dashed__sendy_shipping_method_services', function(Blueprint $table){
-           $table->renameColumn('keendelivery_shipping_method_id', 'sendy_shipping_method_id');
+        Schema::table('dashed__sendy_shipping_method_services', function (Blueprint $table) {
+            $table->renameColumn('keendelivery_shipping_method_id', 'sendy_shipping_method_id');
         });
-        Schema::table('dashed__sendy_shipping_method_service_options', function(Blueprint $table){
-           $table->renameColumn('keendelivery_shipping_method_service_id', 'sendy_shipping_method_service_id');
+        Schema::table('dashed__sendy_shipping_method_service_options', function (Blueprint $table) {
+            $table->renameColumn('keendelivery_shipping_method_service_id', 'sendy_shipping_method_service_id');
         });
 
-        foreach(\Dashed\DashedCore\Models\Customsetting::where('name', 'LIKE', '%keen_delivery%')->get() as $setting){
+        foreach (\Dashed\DashedCore\Models\Customsetting::where('name', 'LIKE', '%keen_delivery%')->get() as $setting) {
             $setting->name = str_replace('keen_delivery', 'sendy', $setting->name);
             $setting->save();
         }
--- routes/SendyRoutes.php
+++ routes/SendyRoutes.php
@@ -1,8 +1,8 @@
 <?php
 
-use Dashed\DashedEcommerceSendy\Controllers\SendyController;
 use Illuminate\Support\Facades\Route;
 use Dashed\DashedCore\Middleware\AdminMiddleware;
+use Dashed\DashedEcommerceSendy\Controllers\SendyController;
 
 Route::middleware(['web', AdminMiddleware::class])->prefix('dashed/sendy')->group(function () {
     Route::get('/download-labels', [SendyController::class, 'downloadLabels'])->name('dashed.sendy.download-labels');
